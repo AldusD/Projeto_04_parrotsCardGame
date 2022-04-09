73052023354244fa8c317d62fac58472717ef71d
AldusD <aldusdanielfsm@gmail.com>
2022-04-09 20:20:36 -0300

    v1.2 - layout completo, funcoes iniciais

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+// declaração de funções
+
 function gameSettings() {
     let numCards;
     let validNumCards;
@@ -9,5 +11,43 @@ function gameSettings() {
             alert("Valor inválido :( \n escolha um número par entre 4 e 14");
         }
     } while(!validNumCards);
+    return numCards;
 }
-gameSettings();
+
+function createCards(numCards) {
+    const cardTypes = numCards / 2;
+    const game = document.querySelector(".container");
+    const cards = [];
+    for(let i = 0; i < cardTypes; i++){
+        // objeto carta
+        const cardA = {
+            seletor: game.querySelector(`.card-${i}.a`),
+            type:  `card-${i}`,
+            id: 'a',
+        }
+        const cardB = {
+            seletor: game.querySelector(`.card-${i}.b`),
+            type:  `card-${i}`,
+            id: 'b',
+        }
+        cards.push(cardA);
+        cards.push(cardB);
+
+        // adicionando ao container
+        game.innerHTML += `<div class="card card-${i} a">
+        <img src="/arquivos/imgs/front.png" alt="papagaio">
+        </div>
+        <div class="card card-${i} b">
+        <img src="/arquivos/imgs/front.png" alt="papagaio">
+        </div>`
+    }
+    console.log(cards, cards[0], cards[1]);
+}
+
+function gameStart() {
+    let numCards = gameSettings();
+    createCards(numCards);
+
+}
+
+gameStart();
