0876f79d17fe94d3d5713c4c74dc114c12402de6
AldusD <aldusdanielfsm@gmail.com>
2022-04-10 11:02:47 -0300

    v1.4 - jogo funcional sem animacoes

--- scripts.js
+++ scripts.js
@@ -1,38 +1,96 @@
+// Variaveis globais
+const game = document.querySelector(".container");
+const cards = [];
+let numCards;
+let finished = 0;
+let moves = 0;
+
 // declaração de funções
-
-function comparador() { 
-	return Math.random() - 0.5; 
-}
-
 function gameSettings() {
-    let numCards;
     let validNumCards;
     // intervalo permitido e par
     do {
-        numCards = prompt("Com quantas cartas você quer jogar?");
+        numCards = Number(prompt("Com quantas cartas você quer jogar?"));
         validNumCards = (numCards >= 4  && numCards <= 14) && numCards % 2 === 0;
         if(!validNumCards) {
             alert("Valor inválido :( \n escolha um número par entre 4 e 14");
         }
     } while(!validNumCards);
-    return numCards;
 }
 
-function createCards(numCards) {
+function flipCard(card) {    
+    // encontrando cartas no js (manter fonte unica de verdade)
+    let cardJs; // carta clicada
+    let openedBeforeJs; // carta clicada anteriormente
+    const openedBefore = game.querySelector(".opened");
+
+    if(!(card.classList.contains("finished"))){
+        for(let i = 0; i < numCards; i++) {
+            if(card === game.querySelector(cards[i].selector)) {
+                cardJs = cards[i];
+            }
+            if(openedBefore === game.querySelector(cards[i].selector)) {
+                openedBeforeJs = cards[i];
+            }
+        }
+        // se for a primeira carta clicada ela caira em else e sera aberta
+        if(openedBefore){
+            // mesmo tipo mas nao a mesma carta => acerto => encerrada
+            if(cardJs.type === openedBeforeJs.type && cardJs.id != openedBeforeJs.id){
+                cardJs.status = "finished";
+                openedBeforeJs.status = "finished";
+                moves += 1;
+                finished += 2;
+            } else if(cardJs.type != openedBeforeJs.type ) {
+                cardJs.status = "closed";
+                openedBeforeJs.status = "closed";
+                moves += 1;
+            }
+        } else {
+            cardJs.status = "opened";
+        }
+        updateCards();   
+        // caso as cartas sejam viradas novamente ou acertadas foi feita uma jogada
+        // caso contrario se trata de cliques superfluo (em cartas já acertadas ou na mesma carta)
+        // ou de primeiro clique
+    }     
+}
+
+function updateCards() {
+    game.innerHTML = "";
+    for(let i = 0; i < numCards; i++) {
+        game.innerHTML += `<div onclick="flipCard(this);" class="card ${cards[i].type} ${cards[i].id} ${cards[i].status}">
+        <img class="image" src="/arquivos/imgs/front.png" alt="papagaio">
+        <img class="gif" src="/arquivos/gifs/${cards[i].type}.gif" alt="papagaio gif">        
+        </div>`;
+        cards[i].selector = `.${cards[i].type}.${cards[i].id}`;
+    }
+    console.log(finished, numCards)
+    if(finished === numCards) {
+        console.log("chegou")
+        alert(`Você venceu em ${moves} jogadas!`);
+    }
+}
+
+function comparador() { 
+	return Math.random() - 0.5; 
+} 
+
+function createCards() {
     const cardTypes = numCards / 2;
-    const game = document.querySelector(".container");
-    const cards = [];
     for(let i = 0; i < cardTypes; i++){
         // objetos carta
         const cardA = {
-            seletor: "",
+            selector: "",
             type:  `card-${i}`,
-            id: 'a',
+            id: 'A',
+            status: "closed"
         }
         const cardB = {
-            seletor: "",
+            selector: "",
             type:  `card-${i}`,
-            id: 'b',
+            id: 'B',
+            status: "closed"
         }
         cards.push(cardA);
         cards.push(cardB);                
@@ -40,19 +98,13 @@ function createCards(numCards) {
     cards.sort(comparador);
 
     // adicionando ao container
-    for(let i = 0; i < numCards; i++) {
-        game.innerHTML += `<div class="card ${cards[i].type} a">
-        <img src="/arquivos/imgs/front.png" alt="papagaio">
-        </div>`
-        cards[i].seletor = `.${cards[i].type}.${cards[i].id}`;
-    }
-    console.log(cards);
+    updateCards();
 }
 
 function gameStart() {
-    let numCards = gameSettings();
-    createCards(numCards);
-
+    gameSettings();
+    createCards();
 }
 
+//codigo
 gameStart();
